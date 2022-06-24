7145b43c01a5b77627a860ab1f93467f920294dd
ASidorovskaya <asidorovskaya@mail.ru>
2022-06-24 21:10:57 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -1,4 +1,15 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -10 до 10.
 # Вывести на экран сумму всех положительных элементов.
 
-# TODO: your code here
+any_list = [-1, -4,  -21]
+
+sum = 0
+
+for element in any_list:
+    if element > 0:
+        sum += element
+
+if sum > 0:
+    print (f"Sum of positive numbers is {sum}")
+else:
+    print ("No positive numbers")
