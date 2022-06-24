e9c27dd0cc6651de71d8a656732bf455bae0681e
ASidorovskaya <asidorovskaya@mail.ru>
2022-06-24 23:41:02 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -4,4 +4,22 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+first_number = int(input("Enter first number: "))     # Первое число
+second_number = int(input("Enter second number: "))    # Второе число
+
+numbers_list = []
+
+if first_number > second_number:
+    first_number, second_number = second_number, first_number
+
+list_number = first_number
+
+while list_number  <= second_number:
+    if list_number % 3 == 0:
+        numbers_list.append(list_number)
+    list_number +=1
+
+if not numbers_list:
+    print ("No numbers that can be divided by 3")
+else:
+    print (numbers_list)
