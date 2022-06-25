6b868b2aa460e04799815c384a7e6d715d573ddd
ASidorovskaya <asidorovskaya@mail.ru>
2022-06-25 23:43:01 +0300

    Update 09_task_tuple.py

--- Module3/practice/list_and_tuple/09_task_tuple.py
+++ Module3/practice/list_and_tuple/09_task_tuple.py
@@ -1,4 +1,15 @@
 # Дано 3 кортежа заполненные произвольными целыми числами.
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
 
-# TODO: your code here
+tup1 = (-2, 2, -9, 12, -5)
+tup2 = (3, -4, 2)
+tup3 = (-2, 3, -4, 12, 2)
+
+
+count = 0
+
+for number in tup1:
+    if number in tup2 and number in tup3:
+        count +=1
+
+print (f"Number of same numbers  = {count}")
