cef32e198d2aca1df38fc644771be0f1873f2c1e
ASidorovskaya <asidorovskaya@mail.ru>
2022-06-27 20:21:41 +0300

    Update 01_task_algo.py

--- Module10/practice/algo/01_task_algo.py
+++ Module10/practice/algo/01_task_algo.py
@@ -4,3 +4,18 @@
 # Вводится число N, далее еще N − 1 чисел: номера оставшихся карточек (различные числа от 1 до N).
 # Программа должна вывести номер потерянной карточки.
 
+cards_number = int(input("Enter number of cards "))
+i = 1
+sum1 = 0
+sum2 = 0
+
+while i <= cards_number-1:
+    sum1 += int (input ("enter card number: "))
+    sum2 = sum2 + i
+    i +=1
+
+sum2 +=i
+
+#print (sum1)
+#print (sum2)
+print (f"Lost card has number {sum2 - sum1}")
